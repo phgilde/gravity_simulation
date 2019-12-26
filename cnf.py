22479295f99268f4f84068956d85d27cdce822cc
pgmcr <ph.gilde@gmail.com>
2019-12-27 00:24:38 +0100

    solar system

--- cnf.py
+++ cnf.py
@@ -1,4 +1,4 @@
-NUM_OF_BODIES = 200
+NUM_OF_BODIES = 3
 WIDTH = 800
 HEIGHT = 800
 WHITE = (255, 255, 255)
@@ -6,13 +6,13 @@ BLACK = (0, 0, 0)
 BLUE = (109, 196, 255)
 MIN_SIZE = 5
 MAX_SIZE = 6
-t = 1
-move_without_render = 1
+t = 0.1
+move_without_render = 10
 
 col_threshold = 2
-bg_alpha = 10
+bg_alpha = 0
 
 max_acc = 50
 
 m0mass = 500
-lock0 = False
+lock0 = True
