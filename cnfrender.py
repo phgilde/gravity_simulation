aa8ae57b135a7c26a79511805f6df1a3d5c14652
pgmcr <ph.gilde@gmail.com>
2020-01-06 22:41:55 +0100

    batch size

--- cnfrender.py
+++ cnfrender.py
@@ -5,10 +5,10 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (109, 196, 255)
 t = 0.001
-move_without_render = 1
+move_without_render = 25
 
 col_threshold = 2
-bg_alpha = 1
+bg_alpha = 100
 
 max_acc = 50
 
@@ -19,3 +19,5 @@ density = 1
 drag_coeff = 1
 
 max_steps = 2000
+
+batch = 200
